41a43135d76fe06a5ba2ad22fb3bc89112b44988
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-09-04 01:39:09 +0600

    route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './Component/Pages/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       
     </div>
   );
--- src/Component/Pages/Header/Header.js
+++ src/Component/Pages/Header/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 const Header = () => {
+    const handleLogin=()=>{
+       console.log('working'); 
+    }
     return (
-        <div>
-            this is header
+        <div className='mt-4'>
+            <ButtonGroup >
+                <Button onClick={handleLogin} variant="secondary">Login</Button>
+                <Button variant="secondary">Register</Button>
+                
+            </ButtonGroup>
         </div>
     );
 };
