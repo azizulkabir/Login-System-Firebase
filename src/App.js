3dd70551c131124abe23fa1cdc19cb06f6f2cfb6
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-09-04 15:38:16 +0600

    shared line

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Component/Home';
+import GoogleLogin from './Component/Pages/Athenication/Login/GoogleLogin';
 import Login from './Component/Pages/Athenication/Login/Login';
 import Register from './Component/Pages/Athenication/Register/Register';
 import Header from './Component/Pages/Header/Header';
+import SharedComponent from './Component/SharedComponent/SharedComponent';
 
 function App() {
   return (
     <div >
       <Header></Header>
       <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/loginbygoogle' element={<GoogleLogin></GoogleLogin>}></Route>
+        <Route path='/shared' element={<SharedComponent></SharedComponent>}></Route>
+        
         <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
       
